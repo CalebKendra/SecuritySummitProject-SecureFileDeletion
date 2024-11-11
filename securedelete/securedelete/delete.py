2ccc682d12a529caa9d43f3fc17227e7d5e9dd63
CalebKendra <calebkendra7@gmail.com>
2024-11-10 21:44:16 -0500

    added new british and dod algorithms

--- securedelete/securedelete/delete.py
+++ securedelete/securedelete/delete.py
@@ -28,18 +28,43 @@ def dod_wipe(file_path: str) -> None:
     """US DoD 5220.22-M (8-306./E, C & E) (7 passes) method for secure deletion."""
     with open(file_path, 'r+b') as f:
         length = os.path.getsize(file_path)
-        for _ in range(7):
-            f.seek(0)
-            f.write(os.urandom(length))
+
+        f.seek(0)
+        f.write(b'\x00' * length)
+
+        f.seek(0)
+        f.write(b'\xFF' * length)
+
+        f.seek(0)
+        f.write(os.urandom(length))
+
+        f.seek(0)
+        f.write(os.urandom(length))
+
+        f.seek(0)
+        f.write(b'\x00' * length)
+
+        f.seek(0)
+        f.write(b'\xFF' * length)
+
+        f.seek(0)
+        f.write(os.urandom(length))
+
     os.remove(file_path)
 
 def hmg_is5_wipe(file_path: str) -> None:
     """British HMG IS5 (Enhanced) (3 passes) method for secure deletion."""
     with open(file_path, 'r+b') as f:
         length = os.path.getsize(file_path)
-        for _ in range(3):
-            f.seek(0)
-            f.write(os.urandom(length))
+        f.seek(0)
+        f.write(b'\x00' * length)
+
+        f.seek(0)
+        f.write(b'\xFF' * length)
+
+        f.seek(0)
+        f.write(os.urandom(length))
+
     os.remove(file_path)
 
 def create_test_file(file_path: str) -> None:
